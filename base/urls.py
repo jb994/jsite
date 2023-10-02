1a4298f5a7d279c05452d52c95112477446d49b0
jb994 <75276925+jb994@users.noreply.github.com>
2023-10-01 23:20:21 -0600

    First Commit

---
+++ base/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('', views.home),
+	path('portfolio', views.portfolio),
+	path('about', views.about),
+	path('contact', views.contact),
+	path('portfolio/singlecell', views.singlecell),
+		path('portfolio/singlecell/cellTypes', views.singlecell_cell_types),
+		path('portfolio/singlecell/volcano-plot', views.volcano_plot),
+		path('portfolio/singlecell/gene-net', views.gene_net),
+	path('portfolio/ml-models', views.ml_models),
+	path('portfolio/splicing', views.splicing),
+	path('portfolio/pathway-analysis', views.pathway_analysis),
+]
---
+++ base/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+	return render(request, 'base/home.html')
+
+def portfolio(request):
+	return render(request, 'base/portfolio.html')
+
+def about(request):
+	return render(request, 'base/about.html')
+
+def contact(request):
+	return render(request, 'base/contact.html')
+
+def singlecell(request):
+	return render(request, 'portfolio/singlecell.html')
+
+def singlecell_cell_types(request):
+	return render(request, 'html_images/Percentage_Cell_Types_unnamed.html')
+def volcano_plot(request):
+	return render(request, 'html_images/Normal_Vehicle_scVolcano.html')
+def gene_net(request):
+	return render(request, 'html_images/NormalxVehicle_2.0lfc_NET.html')
+
+def ml_models(request):
+	return render(request, 'portfolio/ml-models.html')
+
+def splicing(request):
+	return render(request, 'portfolio/splicing-analysis.html')
+
+def pathway_analysis(request):
+	return render(request, 'portfolio/pathway-analysis.html')
